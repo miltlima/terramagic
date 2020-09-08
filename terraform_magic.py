eda3110deef1b35d3c4684c3d5e33072943a0c0d
Milton Lima <milton.lima@outlook.com>
2020-09-07 21:07:14 -0300

    fix bug while loop

--- terraform_magic.py
+++ terraform_magic.py
@@ -1,7 +1,3 @@
-# Name : Project Make dir Terraform
-# Author Milton Lima
-# Release: beta
-# Version: 0.0.1
 
 import os
 import sys
@@ -9,7 +5,6 @@ from pyfiglet import figlet_format
 from colorama import init
 init(strip=not sys.stdout.isatty())
 from termcolor import cprint
-
 
 
 text = "TerraMagic-cli"
@@ -21,17 +16,17 @@ print(f'Voce está no diretório {path}')
 
 def create_dir():
     while True:
-        name_dir = str(input("Informe o nome do projeto que deseja criar: "))
+        name_dir = str(input("Nome do projeto que deseja criar: "))
         if name_dir == '':
             continue
         else:
             try:
                 os.mkdir(name_dir)
             except FileExistsError:
-                print('A pasta do projeto já existe')
+                print('Projeto já existe')
                 continue
-        print(f"Pasta do projeto criada com sucesso {name_dir}")
-        response = str(input("Deseja criar outro ? S/N ou qualquer tecla para sair: "))
+        print(f"{name_dir} criado com sucesso")
+        response = str(input("Deseja criar outro ? S/N"))
         if response == "S" or response == "s":
             create_dir()
         elif response == "N" or response == "n":
@@ -45,12 +40,12 @@ def create_files():
     dir_list = os.listdir('.')
     for file in dir_list:
         print(file)
-    path = str(input("Informe qual Projeto você deseja criar esses arquivos: "))
+    path = str(input("Escolha a pasta para criar os Terraform files: "))
     if path in dir_list:
         os.chdir(path)
         print(f"Arquivos atuais do direrório {os.listdir('.')}")
         terraform_files = ['provider.tf', 'terraform.tfvars', 'backend.tf', 'vars.tf', '.gitignore', 'ec2.tf', 'output.tf']
-        default_files = str(input("Deseja criar os arquivos default Terraform para AWS ? "))
+        default_files = str(input("Criar Terraform files para AWS ? S/N "))
         if default_files == "S" or default_files == 's':
             for filename in terraform_files:
                 all = open(filename, 'w+')
